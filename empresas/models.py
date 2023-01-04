5ed02f07caf5f33df0f46585e9757473622caf3f
Thiago Cavalcante Melo <thiago.melo@transpetro.com.br>
2023-01-04 12:38:28 -0300

    Criação da app Funcionarios

--- empresas/models.py
+++ empresas/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da Empresa')
+
+    def __str__(self):
+        return self.nome
