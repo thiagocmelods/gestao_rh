8d291d807b588ff425fa112c510000381dd111d8
Thiago Cavalcante Melo <thiago.melo@transpetro.com.br>
2023-01-04 13:02:48 -0300

    Criação dos relacionamentos das tabelas

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from departamentos.models import Departamento
+from empresas.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Funcionário')
+    user = models.OneToOneField(User, on_delete=models.PROTECT, unique=True)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
